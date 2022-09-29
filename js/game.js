1bef7fbb155cea8f72180fd135e5990158692c33
msairishik <105224171+msairishik@users.noreply.github.com>
2022-09-29 19:44:37 +0530

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -133,7 +133,7 @@ class Game{
 
                       if(obstacleGroup.isTouching(players)){
                        // gameState = 0;
-                        gameState = 1;
+                       // gameState = 1;
                        // gameState = 2;
                        // gameState = 3;
                       }
@@ -169,8 +169,8 @@ class Game{
     {       
             var x, y;
             //x= 200;
-            x = random(0, width-100);
-           // x = random(0);
+            //x = random(0, width-100);
+            //x = random(0);
             //x = random();
             
             
